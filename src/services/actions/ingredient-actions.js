18da581715dbd70b800d4d50bd35cea3c53a6d88
Kirill Chistov <kirill.v.chistov@yandex.ru>
2023-03-01 12:29:20 +0300

    Создание первых экшенов и редьюсеров

---
+++ src/services/actions/ingredient-actions.js
@@ -0,0 +1,34 @@
+//  Начальные actions для ингридиентов B-Ingredients и B-Constructor  //
+
+import { getIngredients } from "../../utils/api";
+
+//  Получение списка ингредиентов от API  //
+//  Все константы экспортирую, потом, видимо, вынесу в отдельный файл  //
+export const GET_INGREDIENTS_API = "GET_INGREDIENTS_API";
+export const GET_INGREDIENTS_API_OK = "GET_INGREDIENTS_API_OK";
+export const GET_INGREDIENTS_API_FAIL = "GET_INGREDIENTS_API_FAIL";
+
+//  Добавление, удаление данных о просматриваемом ингредиенте (попап с I-Details)  //
+export const OPEN_INGREDIENT_DETAILS = "OPEN_INGREDIENT_DETAILS";
+export const CLOSE_INGREDIENT_DETAILS = "CLOSE_INGREDIENT_DETAILS";
+
+//  Добавил dispatch для получения результата запроса ингридиентов в API  //
+export const dispatchIngredients = () => {
+  return function (dispatch) {
+    dispatch({
+      type: GET_INGREDIENTS_API,
+    });
+    getIngredients().then((res) => {
+      if (res && res.success) {
+        dispatch({
+          type: GET_INGREDIENTS_API_OK,
+          items: res.data,
+        });
+      } else {
+        dispatch({
+          type: GET_INGREDIENTS_API_FAIL,
+        });
+      }
+    });
+  };
+};
